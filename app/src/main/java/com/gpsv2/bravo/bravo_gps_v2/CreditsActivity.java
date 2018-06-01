d60df8201b81742e6695ff6fe53cb973eef1e8be
Samuli <shulkkonen@luukku.com>
2018-06-01 13:33:19 +0300

    first commit

---
+++ app/src/main/java/com/gpsv2/bravo/bravo_gps_v2/CreditsActivity.java
@@ -0,0 +1,25 @@
+package com.gpsv2.bravo.bravo_gps_v2;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+
+        getSupportActionBar().setTitle("Back");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*RelativeLayout relativeLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();*/
+    }
+}
